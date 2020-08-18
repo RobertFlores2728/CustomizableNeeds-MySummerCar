98463db43a6443586af122cd320ca3c736343913
RobertFlores2728 <68761226+RobertFlores2728@users.noreply.github.com>
2020-08-17 20:37:36 -0500

    Added more Console command functionality

--- CustomizableNeeds/CustomizableNeeds/CustomizableNeeds.cs
+++ CustomizableNeeds/CustomizableNeeds/CustomizableNeeds.cs
@@ -1,5 +1,6 @@
 ﻿using MSCLoader;
 using UnityEngine;
+using HutongGames.PlayMaker;
 
 namespace CustomizableNeeds
 {
@@ -9,6 +10,18 @@ namespace CustomizableNeeds
         public override string Name => "CustomizableNeeds"; //You mod name
         public override string Author => "Your Username"; //Your Username
         public override string Version => "1.0"; //Version
+
+
+        //Need Variables
+        float thirst = 0;
+        float hunger = 0;
+        float stress = 0;
+        float urine = 0;
+        float fatigue = 5;
+        float dirtiness = 0;
+
+        GameObject player;
+
 
         // Set this to true if you will be load custom assets from Assets folder.
         // This will create subfolder in Assets folder for your mod.
@@ -22,6 +35,10 @@ namespace CustomizableNeeds
         public override void OnLoad()
         {
             // Called once, when mod is loading after game is fully loaded
+            ConsoleCommand.Add(new CommandGetNeeds());
+
+            
+            
         }
 
         public override void ModSettings()
@@ -44,6 +61,24 @@ namespace CustomizableNeeds
         public override void Update()
         {
             // Update is called once per frame
+            SetPlayMakerValues();
+        }
+
+
+        public void SetPlayMakerValues() {
+            FsmVariables.GlobalVariables.FindFsmFloat("PlayerThirst").Value = thirst;
+            FsmVariables.GlobalVariables.FindFsmFloat("PlayerHunger").Value = hunger;
+            FsmVariables.GlobalVariables.FindFsmFloat("PlayerStress").Value = stress;
+            FsmVariables.GlobalVariables.FindFsmFloat("PlayerUrine").Value = urine;
+            FsmVariables.GlobalVariables.FindFsmFloat("PlayerFatigue").Value = fatigue;
+            FsmVariables.GlobalVariables.FindFsmFloat("PlayerDirtiness").Value = dirtiness;
+        }
+
+
+        public bool CheckPYMKDirtinessDecreased() {
+
+
+            return false;
         }
     }
 }
