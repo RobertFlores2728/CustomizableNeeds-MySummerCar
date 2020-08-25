7f1ae78e56f0d3f7b423b39a1989117a166e1255
RobertFlores2728 <68761226+RobertFlores2728@users.noreply.github.com>
2020-08-24 22:18:36 -0500

    Update CustomizableNeedsGUI.cs

--- CustomizableNeeds/CustomizableNeeds/CustomizableNeedsGUI.cs
+++ CustomizableNeeds/CustomizableNeeds/CustomizableNeedsGUI.cs
@@ -7,11 +7,6 @@ namespace CustomizableNeeds
     {
         public bool guiDisplaying = false;
 
-        GUIStyle guiStyle;
-
-        float backgroundWidth = Screen.width * 0.25f;
-        float backgroundHeight = Screen.height * 0.25f;
-
 
         float sliderValue = 0.0f;
 
@@ -19,23 +14,52 @@ namespace CustomizableNeeds
             if (guiDisplaying == false)
                 return;
 
-            // Starts an area to draw elements
-            GUILayout.BeginArea(new Rect(Screen.width / 2 - backgroundWidth / 2, 0, backgroundWidth, backgroundHeight));
-            GUILayout.Button("Click me");
-            GUILayout.Button("Or me");
+            float areaWidth = 500f;
+            float areaHeight = 2000f;
+
+            // slider style
+            GUIStyle sliderStyle = GUI.skin.GetStyle("HorizontalSlider");
+            sliderStyle.margin = new RectOffset(0, 0, 10, 0);
+
+            //label style
+            GUIStyle labelStyle = GUI.skin.GetStyle("label");
+            labelStyle.margin = new RectOffset(0, 0, 3, 0);
+            labelStyle.fontSize = 12;
+
+            // box over entire area
+            GUI.Box(new Rect(Screen.width / 2 - areaWidth / 2, 0, areaWidth, areaHeight), "Customizable Needs");
+
+            GUILayout.BeginArea(new Rect(Screen.width / 2 - areaWidth / 2, 20, areaWidth, areaHeight));
+
+
+            GUILayout.BeginHorizontal();
+
+
+            GUILayout.BeginVertical();
+            GUILayout.Label("Real - D: 0 H: 0 M: 0 | Game - D: 0 H: 0 M: 0");
+            GUILayout.Label("Real - D: 0 H: 0 M: 0 | Game - D: 0 H: 0 M: 0");
+            GUILayout.EndVertical();
+
+            GUILayout.BeginVertical();
+            sliderValue = GUILayout.HorizontalSlider(sliderValue, 0.0F, 1.0F);
+            sliderValue = GUILayout.HorizontalSlider(sliderValue, 0.0F, 1.0F);
+            sliderValue = GUILayout.HorizontalSlider(sliderValue, 0.0F, 1.0F);
+            sliderValue = GUILayout.HorizontalSlider(sliderValue, 0.0F, 1.0F);
+            sliderValue = GUILayout.HorizontalSlider(sliderValue, 0.0F, 1.0F);
+            sliderValue = GUILayout.HorizontalSlider(sliderValue, 0.0F, 1.0F);
+            GUILayout.EndVertical();
+
+            GUILayout.BeginVertical();
+            GUILayout.Label("Real - D: 0 H: 0 M: 0 | Game - D: 0 H: 0 M: 0");
+            GUILayout.Label("Real - D: 0 H: 0 M: 0 | Game - D: 0 H: 0 M: 0");
+            GUILayout.EndVertical();
+
+
+            GUILayout.EndHorizontal();
+
+
             GUILayout.EndArea();
 
-            /*
-            GUI.Box(new Rect(Screen.width / 2 - backgroundWidth/2, 0, backgroundWidth, backgroundHeight), "CustomizableNeeds");
-
-            sliderValue = GUI.HorizontalSlider(new Rect(Screen.width / 2 - backgroundWidth / 2, 0, 100, 30), sliderValue, 0.0F, 1.0F);
-            */
-
-        }
-
-
-        public void SetupGuiStyle() {
-            //guiStyle.padding = 10;
         }
 
 
