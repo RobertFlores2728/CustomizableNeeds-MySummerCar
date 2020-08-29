9221acadcd35aca6b37ba94060eebc7b1d5b9aa0
RobertFlores2728 <68761226+RobertFlores2728@users.noreply.github.com>
2020-08-28 19:25:59 -0500

    Update CustomizableNeedsGUI.cs

--- CustomizableNeeds/CustomizableNeeds/CustomizableNeedsGUI.cs
+++ CustomizableNeeds/CustomizableNeeds/CustomizableNeedsGUI.cs
@@ -11,123 +11,122 @@ namespace CustomizableNeeds
         float sliderValue = 0.0f;
 
         public void OnGUI()
+    {
+        if (guiDisplaying == false)
+            return;
+
+        DrawIncreaseRates();
+
+        DrawDecreaseRates();
+
+        
+    }
+
+    public struct Coords
+    {
+        public Coords(float x, float y)
         {
-            if (guiDisplaying == false)
-                return;
-
-            DrawIncreaseRates();
-
-            DrawDecreaseRates();
-
-
+            this.x = x;
+            this.y = y;
         }
 
+        public float x;
+        public float y;
+    }
 
-        void DrawIncreaseRates()
-        {
-            float areaWidth = 850f;
-            float areaHeight = 210f;
+    void DrawBox() {
+        Coords boxDimensions = new Coords(850f, 210f); // keep box dimensions within (1280, 720) pixels
+        Coords boxPosition = new Coords(Screen.width / 2 - boxDimensions.x / 2, 20);
+        Rect boxRect = new Rect(boxPosition.x, boxPosition.y, boxDimensions.x, boxDimensions.y);
 
-            float area1PositionX = Screen.width / 2 - areaWidth / 2;
-            float area1PositionY = 20;
+        GUI.Box(boxRect, "Customizable Needs");
+    }
 
+    void DrawLabel(float posX, float posY, string labelText, int fontSize = 12) {
+        GUIStyle labelStyle = new GUIStyle(GUI.skin.GetStyle("Label"));
+        labelStyle.fontSize = fontSize;
 
+        Coords labelDimensions = new Coords(250f, 25f);
+        Coords labelPosition = new Coords(posX, posY);
+        Rect labelRect = new Rect(labelPosition.x, labelPosition.y, labelDimensions.x, labelDimensions.y); ;
 
-
-            // slider style
-            GUIStyle sliderStyle = new GUIStyle(GUI.skin.GetStyle("HorizontalSlider"));
-            sliderStyle.margin = new RectOffset(0, 0, 13, 0);
-
-            //label style
-            GUIStyle labelStyle = new GUIStyle(GUI.skin.GetStyle("Label"));
-            labelStyle.margin = new RectOffset(0, 0, 12, 0);
-            labelStyle.padding = new RectOffset(50, 0, 0, 0);
-
-            // box over entire area
-            GUI.Box(new Rect(area1PositionX, area1PositionY - 20, areaWidth, areaHeight), "Customizable Needs");
-
-            GUILayout.BeginArea(new Rect(area1PositionX, area1PositionY, areaWidth, areaHeight));
-
-            labelStyle.fontSize = 15;
-            GUILayout.Label("Increase Rates:", labelStyle);
-
-            labelStyle.fontSize = 13;
-            GUILayout.BeginHorizontal();
-            GUILayout.Label("Need", labelStyle);
-            GUILayout.Label("Rate", labelStyle);
-            GUILayout.Label("Estimated time from empty to full", labelStyle);
-            GUILayout.EndHorizontal();
+        GUI.Label(labelRect, labelText, labelStyle);
+    }
 
 
-            labelStyle.fontSize = 12;
-            GUILayout.BeginHorizontal();
-            GUILayout.Label("Need 1", labelStyle);
-            sliderValue = GUILayout.HorizontalSlider(sliderValue, 0.0F, 1.0F, sliderStyle, GUI.skin.GetStyle("horizontalSliderThumb"), GUILayout.MaxWidth(240f));
-            GUILayout.Label("Real - D: 0 H: 0 M: 0 S: 0 || Game - D: 0 H: 0 M: 0 S: 0", labelStyle);
-            GUILayout.EndHorizontal();
+    //hardcode positions only way!
+    // keep all positions within (1280, 720) pixels
+    void DrawIncreaseRates() {
 
-            labelStyle.fontSize = 12;
-            GUILayout.BeginHorizontal();
-            GUILayout.Label("Need 1", labelStyle);
-            sliderValue = GUILayout.HorizontalSlider(sliderValue, 0.0F, 1.0F, sliderStyle, GUI.skin.GetStyle("horizontalSliderThumb"), GUILayout.MaxWidth(240f));
-            GUILayout.Label("Never", labelStyle);
-            GUILayout.EndHorizontal();
+        float centerX = Screen.width / 2;
 
+        //BACKGROUND BOX
+        DrawBox();
 
+        //SECTION LABEL
+        DrawLabel(centerX - 50, 50, "Increase Rates", 15);
 
-            GUILayout.EndArea();
-        }
+        //COLUMN LABELS
+        DrawLabel(centerX - 325, 75, "Need", 13);
+        DrawLabel(centerX - 50, 75, "Rate", 13);
+        DrawLabel(centerX + 150, 75, "Estimated time from empty to full", 13);
 
+        //ROW 1
+        DrawLabel(centerX - 325, 100, "Thirst", 12);
+        DrawLabel(centerX - 50, 100, "Rate", 12);
+        DrawLabel(centerX + 150, 100, "Estimated time from empty to full", 12);
 
-        void DrawDecreaseRates()
-        {
-            float areaWidth = 850f;
-            float areaHeight = 210f;
-
-            float area1PositionX = Screen.width / 2 - areaWidth / 2;
-            float area1PositionY = 300;
+    }
 
 
+    void DrawDecreaseRates() {
+        float areaWidth = 850f;
+        float areaHeight = 210f;
+
+        float area1PositionX = Screen.width / 2 - areaWidth / 2;
+        float area1PositionY = 300;
+
+        
 
 
-            // slider style
-            GUIStyle sliderStyle = new GUIStyle(GUI.skin.GetStyle("HorizontalSlider"));
-            sliderStyle.margin = new RectOffset(0, 0, 13, 0);
+        // slider style
+        GUIStyle sliderStyle = new GUIStyle(GUI.skin.GetStyle("HorizontalSlider"));
+        sliderStyle.margin = new RectOffset(0, 0, 13, 0);
 
-            //label style
-            GUIStyle labelStyle = new GUIStyle(GUI.skin.GetStyle("Label"));
-            labelStyle.margin = new RectOffset(100, 0, 12, 0);
-            labelStyle.padding = new RectOffset(50, 0, 0, 0);
+        //label style
+        GUIStyle labelStyle = new GUIStyle(GUI.skin.GetStyle("Label"));
+        labelStyle.margin = new RectOffset(0, 0, 12, 0);
+        //labelStyle.padding = new RectOffset(50, 0, 0, 0);
 
-            // box over entire area
-            GUI.Box(new Rect(area1PositionX, area1PositionY - 20, areaWidth, areaHeight), "Customizable Needs");
+        // box over entire area
+        GUI.Box(new Rect(area1PositionX, area1PositionY - 20, areaWidth, areaHeight), "Customizable Needs");
 
-            GUILayout.BeginArea(new Rect(area1PositionX, area1PositionY, areaWidth, areaHeight));
+        GUILayout.BeginArea(new Rect(area1PositionX, area1PositionY, areaWidth, areaHeight));
 
-            labelStyle.fontSize = 15;
-            GUILayout.Label("Decrease Rates:", labelStyle);
+        labelStyle.fontSize = 15;
+        GUILayout.Label("Decrease Rates:", labelStyle);
 
-            labelStyle.fontSize = 13;
-            GUILayout.BeginHorizontal();
-            GUILayout.Label("Need", labelStyle);
-            GUILayout.Label("Rate", labelStyle);
-            GUILayout.Label("Value", labelStyle);
-            GUILayout.EndHorizontal();
+        labelStyle.fontSize = 13;
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Need", labelStyle);
+        GUILayout.Label("Rate", labelStyle);
+        GUILayout.Label("Value", labelStyle);
+        GUILayout.EndHorizontal();
 
 
-            labelStyle.fontSize = 12;
-            GUILayout.BeginHorizontal();
-            GUILayout.Label("Need 1", labelStyle);
-            sliderValue = GUILayout.HorizontalSlider(sliderValue, 0.0F, 1.0F, sliderStyle, GUI.skin.GetStyle("horizontalSliderThumb"), GUILayout.MaxWidth(240f));
-            labelStyle.padding = new RectOffset(83, 0, 0, 0);
-            GUILayout.Label("1.0", labelStyle);
-            labelStyle.padding = new RectOffset(50, 0, 0, 0);
-            GUILayout.EndHorizontal();
+        labelStyle.fontSize = 12;
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Need 1", labelStyle);
+        decreaseValue = GUILayout.HorizontalSlider(decreaseValue, 0.0F, 1.0F, sliderStyle, GUI.skin.GetStyle("horizontalSliderThumb"), GUILayout.MaxWidth(240f));
+        //labelStyle.padding = new RectOffset(83, 0, 0, 0);
+        GUILayout.Label(decreaseValue.ToString(), labelStyle);
+        //labelStyle.padding = new RectOffset(50, 0, 0, 0);
+        GUILayout.EndHorizontal();
 
 
 
-            GUILayout.EndArea();
-        }
+        GUILayout.EndArea();
+    }
 
 
         //calculates the time needed to fill attribute bar starting from 0 to 100, in real and game time
